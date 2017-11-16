0655d88b26fa4e6c5e35d2564def97e35d253620
이민철 <herren.lmc@gmail.com>
2017-11-16 10:34:14 +0900

    celeryconfig fix

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,2 +1,2 @@
-broker_url = 'amqp://{0}:{1}@{2}:5672/{3}'.format('node1', 'node', 'http://ec2-13-125-39-17.ap-northeast-2.compute.amazonaws.com', 'crawling')
+broker_url = 'amqp://{0}:{1}@{2}:5672/{3}'.format('node1', 'node', 'ec2-13-125-39-17.ap-northeast-2.compute.amazonaws.com', 'crawling')
 timezone = 'Asia/Seoul'
