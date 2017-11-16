9428ded4d29ff570135c71182f18cc210d4477b0
이민철 <herren.lmc@gmail.com>
2017-11-16 10:38:15 +0900

    update: requirements

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 from celery import Celery
 from celery.task import Task
+import requests
+from bs4 import BeautifulSoup
 
 import celeryconfig
 
 app = Celery()
 app.config_from_object(celeryconfig)
+
